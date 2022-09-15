fd8e19f2848ba39380aeb55520d56bab6994d56f
Nguyen Giang Chuong <42799279+ngchuong@users.noreply.github.com>
2022-09-15 09:02:29 +0700

    Add files via upload

---
+++ morning.bat
@@ -0,0 +1,6 @@
+start chrome.exe 
+start chrome.exe -incognito "https://www.tirp2.axnavi.com/demo/token"
+start outlook.exe
+start Code
+start notepad++.exe 
+start C:\Users\chuongng\AppData\Local\Microsoft\Teams\Update.exe --processStart "Teams.exe"
